d23815ccacaf0a6dc2445bdbb90b780026e2e93e
LukBukkit <luk.bukkit@gmail.com>
2018-10-20 16:45:21 +0200

    Adds a hello index controller

---
+++ src/Controller/HelloIndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloIndexController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response(
+            '<!DOCTYPE HTML><html><body>Hello</body></html>'
+        );
+    }
+}
