18546005c19c5cd4803ed8303bef8cdbfb3b77cc
LukBukkit <luk.bukkit@gmail.com>
2019-12-19 13:47:40 +0100

    Upgrading to Symfony 4.4

--- src/Repository/CafeteriaMealRepository.php
+++ src/Repository/CafeteriaMealRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\CafeteriaMeal;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method CafeteriaMeal|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class CafeteriaMealRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CafeteriaMeal::class);
     }
